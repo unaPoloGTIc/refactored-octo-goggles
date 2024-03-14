b3f48a04d92a209ed857b59b65def85d1bb46ed2
unaPoloGTIc <unapologtic@gmail.com>
2024-03-14 22:14:15 +0200

    Hoping that a future commit will enjoy a good laugh over here.

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+//import Iframe from 'react-iframe';
+import Iframe from 'react-native'; 
+
+//import { Div } from 'react-native-div' ;
+
+import React from 'react';
+const GoogleDocumentViewer = () => {
+    const documentURL = 'https://docs.google.com/spreadsheets/d/19kiKs-D4MoO-_hxRosJDFAmuj7E2_ZmUMWtyUForveA/';
+  return (
+   <View>
+      <Iframe src={documentURL} width="100%" height="600px"/>
+    </View>
+  );
+};
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>looks much newer in here</Text>
-      <StatusBar style="auto" />
-    </View>
+    return (
+      <View>
+	  <Text>sigh
+	      style=(styles.container)
+	  </Text>
+	  {/*GoogleDocumentViewer()*/ /*I had to duckduckgo how to add a comment here, and then it made sense.*/}
+
+      </View>
   );
 }
 
